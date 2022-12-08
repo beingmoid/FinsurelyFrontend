b75dd4c3cd4da3399b8e63039bd592895c46d080
ShaikhHamzaHafeez <hamzahafeez980@gmail.com>
2022-12-08 06:14:45 +0500

    aa

--- src/app/pages/transactions/payment/payment.component.ts
+++ src/app/pages/transactions/payment/payment.component.ts
@@ -4,6 +4,8 @@ import { Payment } from 'src/app/models/refundDTO';
 import { AlertService } from 'src/app/services/alert.service';
 import { PaymentService } from 'src/app/services/APIServices/payment-service.service';
 import { SharedService } from 'src/app/services/shared.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 class FilterObject {
   constructor(private data: Payment) { }
 
@@ -16,14 +18,67 @@ class FilterObject {
 export class PaymentComponent implements OnInit {
 
   paymentSub:Subject<Payment> = new Subject<Payment>();
+  form: FormGroup;
+
   constructor(private sharedService:SharedService,
     private alert:AlertService,
-    private PaymentService:PaymentService) {
+    private PaymentService:PaymentService,
+    private fb: FormBuilder) {
 
     this.sharedService.closeForm.subscribe(res=>{
       this.modalView=false;
     });
+    this.form = this.fb.group({
+      dateFrom: new FormControl(null),
+      dateTo: new FormControl(null),
+      branch: new FormControl(null),
+      isPdf: new FormControl(null),
+      isExcel: new FormControl(null)
+
+    })
+
    }
+
+   searchAddress: string;
+  // listData: Branch[];
+  nameList = [
+    { text: 'Export as PDF', value: 'PDF', checked: true },
+    { text: 'Export as Excel', value: 'Excel', checked: false }
+  ];
+
+  data = [
+    {
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      name: 'Jim Red',
+      age: 32,
+      address: 'London No. 2 Lake Park'
+    }
+  ];
+  displayData = [...this.data];
+  sortName = null;
+  sortValue = null;
+  listOfSearchName = [];
+
+  show = false;
+
+  chiplist = [];
+
+
+  
   search:string;
   listData:any[]=[];
   pageSize=20;
--- src/app/pages/transactions/transfer/transfer.component.ts
+++ src/app/pages/transactions/transfer/transfer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Payment } from 'src/app/models/refundDTO';
 import { SharedService } from 'src/app/services/shared.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 class FilterObject {
   constructor(private data: Payment) { }
 
@@ -11,12 +13,61 @@ class FilterObject {
   styleUrls: ['./transfer.component.scss']
 })
 export class TransferComponent implements OnInit {
-  constructor(private sharedService:SharedService) {
+  form: FormGroup;
+
+  constructor(private sharedService:SharedService, private fb: FormBuilder) {
 
     this.sharedService.closeForm.subscribe(res=>{
       this.modalView=false;
     });
+    this.form = this.fb.group({
+      dateFrom: new FormControl(null),
+      dateTo: new FormControl(null),
+      branch: new FormControl(null),
+      isPdf: new FormControl(null),
+      isExcel: new FormControl(null)
+
+    })
+
    }
+   searchAddress: string;
+  // listData: Branch[];
+  nameList = [
+    { text: 'Export as PDF', value: 'PDF', checked: true },
+    { text: 'Export as Excel', value: 'Excel', checked: false }
+  ];
+
+  data = [
+    {
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      name: 'Jim Red',
+      age: 32,
+      address: 'London No. 2 Lake Park'
+    }
+  ];
+  displayData = [...this.data];
+  sortName = null;
+  sortValue = null;
+  listOfSearchName = [];
+
+  show = false;
+
+  chiplist = [];
+
   search:string;
   listData:any[]=[];
   pageSize=20;
