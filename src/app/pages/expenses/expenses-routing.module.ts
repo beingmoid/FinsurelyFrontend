55bbbf0841ccb9626838a4549842f927e5690bea
Moid <me_moid@hotmail.com>
2022-12-10 17:07:56 -0600

    aa

--- src/app/pages/expenses/expenses-routing.module.ts
+++ src/app/pages/expenses/expenses-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExpensesModule } from './expenses.module';
+import { ViewExpensesComponent } from './view-expenses/view-expenses.component';
 
 const routes: Routes = [{
   path: '',
-  component: ExpensesModule,
+  component:ViewExpensesComponent ,
 }];
 
 @NgModule({
