19ce6516378564eed7ebcb939f7d4f63699d19e6
moid <me_moid@hotmail.com>
2022-12-11 14:11:25 -0600

    aa

---
+++ src/app/pages/expenses/add-receipts/add-receipts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-receipts',
+  templateUrl: './add-receipts.component.html',
+  styleUrls: ['./add-receipts.component.scss']
+})
+export class AddReceiptsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
